744d95dd4ce9fb5c96811b034d64960b8284481a
vmorozov893 <vmorozov893@gmail.com>
2024-07-12 19:44:24 +0300

    начало

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome");
+        System.out.println("Задание 1\n");
+        int apple = 123456;
+        byte cat = 123;
+        short dog = 12345;
+        long cup = 123456789;
+        float literWater = 123.456f;
+        double kilogramGold = 1234.435784;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        System.out.println("Значение переменной cat с типом byte равно " + cat);
+        System.out.println("Значение переменной dog с типом short равно " + dog);
+        System.out.println("Значение переменной cup с типом long равно " + cup);
+        System.out.println("Значение переменной literWater с типом float равно " + literWater);
+        System.out.println("Значение переменной kilogramGold с типом double равно " + kilogramGold);
     }
 }
